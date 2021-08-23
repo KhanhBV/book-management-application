b57938690cf9ce348e74a1a35200c83c8e541e42
khanhBV <khanhBVSE@gmail.com>
2021-08-23 21:38:51 +0700

    Init Application

---
+++ BookManagementAppliction/BookManagementAppliction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookManagementAppliction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
